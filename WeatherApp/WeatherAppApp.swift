0cd39d59dd85e5a6dee8ded2e385d93b6a12decc
franknfjr <frankdonascimentojr@gmail.com>
2022-05-01 16:40:54 -0300

    first commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Frank Ferreira on 01/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
